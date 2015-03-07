6601957fe14d6c982d745d555c46c1b32355f5e3
leguest <githubuest@gmail.com>
2015-03-07 23:59:16 +0300

    Добавил выбор препроцессоров

--- app/index.js
+++ app/index.js
@@ -1,62 +1,193 @@
-'use strict';
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
+"use strict";
+var util = require('util'); // Какая-то неизвестная зависимость
+var path = require('path'); // Какая-то неизвестная зависимость
+var yeoman = require('yeoman-generator'); // Подключаем генератор yeoman
 var yosay = require('yosay');
+var chalk = require('chalk');
 
-module.exports = yeoman.generators.Base.extend({
-  initializing: function () {
-    this.pkg = require('../package.json');
-  },
+var Legen = module.exports = function Legen(args, options, config)
+{
+	yeoman.generators.Base.apply(this, arguments);
 
-  prompting: function () {
-    var done = this.async();
+	this.on('end', function()
+		{
+			this.installDependencies({ skipInstall: options['skip-install']});
+		}
+	);
+	// преобразуем package.json в строку и заносим его в инстанс pkg
+	this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+// Производим наследованием в util?
+util.inherits(Legen, yeoman.generators.Base);
 
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the fine ' + chalk.red('Legen') + ' generator!'
-    ));
+// Генерируем вопросы
+Legen.prototype.askFor = function askFor()
+{
+	// Вызываем инстансы асинхронно
+	var cb = this.async();
+	var prompts = [
+		{
+			type: 'input',
+			name: 'appName',
+			message: 'Введите название вашего проекта:',
+		},
+		// В разработке
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+		// {
+		// 	type: 'list',
+		// 	name: 'CSSframeworks',
+		// 	message: 'Какой Сборщик проектов предпочитаете?',
+		// 	choices: 
+		// 	[
+		// 		{
+		// 			name: 'Grunt',
+		// 			value: 'grunt'
+		// 		},
+		// 		{
+		// 			name: 'Gulp',
+		// 			value: 'gulp'
+		// 		},
+		// 		{
+		// 			name: 'Никакой',
+		// 			value: 'null'
+		// 		}
+		// 	],
+		// 	default: 1
+		// },
+		//  В разработке
+		// {
+		// 	type: 'confirm',
+		// 	name: 'useFramework',
+		// 	message: 'Вы будете использовать фрейморки?',
+		// 	default: true
+		// },
+		// В разработке
+		// {
+		// 	type: 'list',
+		// 	name: 'CSSframeworks',
+		// 	message: 'Какой CSS-фреймворк будете использовать?',
+		// 	choices: 
+		// 	[
+		// 		// В разработке
+		// 		{
+		// 			name: 'Foundation 5',
+		// 			value: 'foundation'
+		// 		},
+		// 		{
+		// 			name: 'Twitter Bootstrap 3',
+		// 			value: 'bootstrap'
+		// 		},
+		// 		{
+		// 			name: 'Никакой',
+		// 			value: 'null'
+		// 		}
+		// 	],
+		// 	default: 0
+		// },
+		{
+			type: 'list',
+			name: 'CSSpreprocessors',
+			message: 'Какой CSS-препроцессор будете использовать?',
+			choices: 
+			[
+				{
+					name: 'Stylus',
+					// препроцессор, расширение
+					value: ['stylus', 'styl']
+				},
+				{
+					name: 'SCSS',
+					// препроцессор, расширение
+					value: ['sass', 'scss']
+				},
+				// В разработке
+				{
+					name: 'LESS',
+					// препроцессор, расширение
+					value: ['less', 'less']
+				},
+				{
+					name: 'Никакой',
+					value: ['null']
+				}
+			],
+			default: 0
+		}
+	];
 
-    this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
+	this.prompt(prompts, function(answers)
+	{
+		this.appName = answers.appName;
+		// Что за CSS фреймворк мы выбрали
+		this.CSSframeworks = answers.CSSframeworks;
+		// Что за CSS препроцессор мы выбрали
+		this.CSSpreprocessorsName = answers.CSSpreprocessors[0];
+		// расширение CSS препроцессора
+		this.CSSpreprocessorsExt = answers.CSSpreprocessors[1];
+		
 
-      done();
-    }.bind(this));
-  },
+		console.log(this.CSSpreprocessorsExt);
 
-  writing: {
-    app: function () {
-      this.fs.copy(
-        this.templatePath('_package.json'),
-        this.destinationPath('package.json')
-      );
-      this.fs.copy(
-        this.templatePath('_bower.json'),
-        this.destinationPath('bower.json')
-      );
-    },
+		cb();
+	}.bind(this));
+};
+Legen.prototype.app = function app() 
+{
+	// Создаем сам проект
+	this.mkdir('app');
 
-    projectfiles: function () {
-      this.fs.copy(
-        this.templatePath('editorconfig'),
-        this.destinationPath('.editorconfig')
-      );
-      this.fs.copy(
-        this.templatePath('jshintrc'),
-        this.destinationPath('.jshintrc')
-      );
-    }
-  },
 
-  install: function () {
-    this.installDependencies({
-      skipInstall: this.options['skip-install']
-    });
-  }
-});
+	// Создаем папку с исходниками
+	this.mkdir('app/src');
+		// Папка для препроцессоров CSS
+	this.mkdir('app/src/'+this.CSSpreprocessorsName+'/');
+		// Папка для препроцессоров HTML
+	this.mkdir('app/src/jade/');
+		// Coffescript
+	this.mkdir('app/src/coffee/');
+
+
+	// Создаем папку с версией для разработки
+	this.mkdir('app/dev');
+		// Папка для css
+	this.mkdir('app/dev/css');
+			// Библиотеки css
+	this.mkdir('app/dev/css/libs');
+		// Папка для js
+	this.mkdir('app/dev/js');
+			// Библиотеки js
+	this.mkdir('app/dev/js/libs');
+		// Папка для картинок
+	this.mkdir('app/dev/img');
+		// Папка для шрифтов
+	this.mkdir('app/dev/fonts');	
+
+
+
+	// Папка для продакшена
+	this.mkdir('app/build');
+		// Папка для css
+	this.mkdir('app/build/css');
+			// Библиотеки css
+	this.mkdir('app/build/css/libs');
+		// Папка для js
+	this.mkdir('app/build/js');
+			// Библиотеки js
+	this.mkdir('app/build/js/libs');
+		// Папка для картинок
+	this.mkdir('app/build/img');
+		// Папка для шрифтов
+	this.mkdir('app/build/fonts');
+
+	// Генерирует пакет с зависимостями
+	this.copy('_package.json', 'package.json');
+	// бовер с фреймворками и библиотеками
+  	this.copy('_bower.json', 'bower.json');
+  	// Gulp file меняющийся от полученных данных о препроцессорах
+  	this.copy('_Gulpfile.js', 'Gulpfile.js');
+}
+
+Legen.prototype.projectfiles = function projectfiles() {
+  this.copy('editorconfig', '.editorconfig');
+  this.copy('jshintrc', '.jshintrc');
+};
---
+++ app/templates/_Gulpfile.js
@@ -0,0 +1,96 @@
+// Generated on <%= (new Date).toISOString().split('T')[0] %> using <%= pkg.name %> <%= pkg.version %>
+'use strict';
+// Инициализируем плагины
+var gulp    = require('gulp'), // Сообственно Gulp JS
+    css     = require('gulp-<%= CSSpreprocessorsName %>'), // CSS preprocessor
+    jade    = require('gulp-jade'), // Jade
+    coffee  = require('gulp-coffee'), // coffee-script
+    csso    = require('gulp-csso'), // Минификация CSS
+    uglify  = require('gulp-uglify'), // Минификация JS
+    concat  = require('gulp-concat'), // Склейка файлов
+    connect = require('gulp-connect'); // http-сервер
+
+
+
+<% if (_.slugify(CSSpreprocessorsName) != 'null') { %>
+        //Вариативный таск 
+// Собираем CSS препроцессор
+gulp.task('css', function () {
+    gulp.src('./app/src/<%= _.slugify(CSSpreprocessorsName) %>/**/*.<%= CSSpreprocessorsExt %>')
+        .pipe(stylus()) // Преобразуем в css
+        .on('error', console.log) // Если есть ошибки, выводим и продолжаем
+        .pipe(concat('style.css')) // склеиваем полученные css в style.css
+        .pipe(gulp.dest('./app/dev/css')) // отправляем в версию для разработки
+        .pipe(connect.reload());
+});
+<% } %>
+        //Вариативный таск
+// Собираем HTML препроцессор
+gulp.task('jade', function() {
+    gulp.src(['./app/src/jade/**/*.jade', '!./app/src/jade/utils/*.jade'])
+        .pipe(jade({
+            pretty: true
+        }))  // Собираем Jade только в папке assets/template/ исключая файлы из utils
+        .on('error', console.log) // Если есть ошибки, выводим и продолжаем
+    .pipe(gulp.dest('./app/dev/')) // отправляем в версию для разработки
+    .pipe(connect.reload()); // даем команду на перезагрузку страницы
+}); 
+        //Вариативный таск
+// Собираем JS
+gulp.task('js', function() {
+    gulp.src(['./app/src/coffee/**/*.coffee'])
+        .pipe(coffee({bare: true}))
+        .on('error', console.log)
+        //.pipe(concat('script.js')) // Склеиваем JS
+        .pipe(gulp.dest('./app/dev/js'))
+        .pipe(connect.reload()); // даем команду на перезагрузку страницы
+});
+ 
+        //Неизменный таск
+// Локальный сервер для разработки
+gulp.task('http-server', function() {
+    connect.server(
+    {
+        root: 'app/dev',
+        livereload: true
+    });
+});
+        // Неизменный таск
+// Смотрим за изменениями
+gulp.task('watch', function () {
+    gulp.watch(['./app/src/<%= _.slugify(CSSpreprocessorsName) %>/**/*.<%= CSSpreprocessorsExt %>'], ['css']);
+    gulp.watch(['./app/src/jade/**/*.jade'], ['jade']);  
+    gulp.watch(['./app/src/coffee/**/*.coffee'], ['js']);
+});
+
+        // Неизменный таск
+// Запуск сервера разработки gulp watch
+gulp.task('default', ['http-server', 'watch']);
+ 
+        // Вариативный таск
+// Запуск сборщика продакшена
+gulp.task('build', function() {
+ 
+    // CSS preprocessor
+ 
+    gulp.src(['./app/src/<%= _.slugify(CSSpreprocessorsName) %>/**/*.<%= CSSpreprocessorsExt %>'])
+        .pipe(css()) // преобразуем sass в css
+        .pipe(concat('style.css')) // склеиванием css
+        // .pipe(csso()) // минимизируем css
+        .pipe(gulp.dest('./app/build/css/')); // записываем css
+ 
+    // jade
+ 
+    gulp.src(['./app/src/jade/**/*.jade', '!./app/assets/jade/utils/**/*.jade'])
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./app/build/'));
+ 
+    // js
+ 
+    gulp.src(['./app/src/coffee/**/*.coffee', './app/assets/coffee/**/*.js'])
+        .pipe(coffee({bare: true}))
+        // .pipe(uglify())
+        .pipe(gulp.dest('./app/build/js'));
+});
